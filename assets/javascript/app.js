92af9537f433a771b5f56eed399b1ebd3fc0119d
Jordan Reynolds <jordanreynolds34@gmail.com>
2017-07-06 19:22:20 -0600

    All Database Functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
  // hides the main container  
-  $(window).on( "load", function(){
-    $(".mainContain").hide();
-    $(".newStory").hide();
-    $(".joinStory").hide();
-    $(".readStory").hide();
+ $(window).on( "load", function(){
+  $(".mainContain").hide();
+  $(".newStory").hide();
+  $(".joinStory").hide();
+  $(".readStory").hide();
 
-  })
+})
 
 
   // removes landing page stuff and shows main container
-  $("#kickassbtn").on("click", function(event){
+  $("#editStoryButt").on("click", function(event){
     $(".mainContain").show();
     $(".landingPage").hide();
     $(".joinStory").hide();
@@ -17,30 +17,31 @@
   })
 
   // New Story On Click Function
-    $(".newStoryButt").click(function(event){
-      $(".newStory").show();
-      $(".landingPage").hide();
-      $(".mainContain").hide();
-      $(".joinStory").hide();
-      $(".readStory").hide();
-    })
+  $(".newStoryButt").click(function(event){
+    $(".newStory").show();
+    $(".landingPage").hide();
+    $(".mainContain").hide();
+    $(".joinStory").hide();
+    $(".readStory").hide();
+  })
 
   // Join Story Button Function
-    $(".joinStoryButt").click(function(event){
-      $(".joinStory").show();
-      $(".landingPage").hide();
-      $(".mainContain").hide();
-      $(".newStory").hide();
-      $(".readStory").hide();
-});
+  $(".joinStoryButt").click(function(event){
+    $(".joinStory").show();
+    $(".landingPage").hide();
+    $(".mainContain").hide();
+    $(".newStory").hide();
+    $(".readStory").hide();
+  });
 
  // Read Story Button Function
-    $(".readStoryButt").click(function(event){
-      $(".readStory").show();
-      $(".landingPage").hide();
-      $(".mainContain").hide();
-      $(".newStory").hide();
-      $(".joinStory").hide();
+ $(".readStoryButt").click(function(event){
+
+  $(".readStory").show();
+  $(".landingPage").hide();
+  $(".mainContain").hide();
+  $(".newStory").hide();
+  $(".joinStory").hide();
 });
 
 
@@ -63,7 +64,8 @@
     var adLibArray = [];
     var adLib;
     var storyPrompt;
-    var storyIndex = 0;
+    var storyIndex;
+    var storyPath;
 
     // Capture Button Click
     $(".commit-Lib").on("click", function(event) {
@@ -73,7 +75,7 @@
       //Get the User Input and Trim the spaces.
       adLib = $(".adLib-input").val().trim();
       console.log($(".adLib-input").val().trim())
-      adLibArray.push(adLib);
+      // adLibArray.push(adLib);
       
       
       // Code to push the data to 
@@ -81,23 +83,19 @@
       storiesRef.once("value", function(snapshot) {
         var stories = snapshot.val();
         var currenStory = stories[storyIndex];
-        // if(storyIndex = "newStory"){
-        //   storyIndex = Object.keys(storiesRef).length+1
-        //   console.log(storyIndex)
-          // }
-
-        // else if(currenStory){
+        
           var storySentences = currenStory.adLibArray;
+          var storyPrompt = currenStory.storyPrompt
           storySentences.push(adLib);
-
-          storiesRef.child(storyIndex).set({
-            id: storyIndex,
-            name: name,
-            email: email,
-            age: age,
-            adLibArray: storySentences,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-          });
+          console.log(storyPrompt);
+          
+          storiesRef.child(storyIndex+"/adLibArray").set(storySentences);
+          // {
+            // id: storyIdNumber,
+            // storyPrompt: storyPrompt,
+            // adLibArray: storySentences
+            // dateAdded: firebase.database.ServerValue.TIMESTAMP
+          // });
 
           if (storySentences.length > 0){
            var lastSentence = storySentences.length-1;
@@ -113,7 +111,8 @@
       $(".adLib-input").val("");
       $(".prompt-Lib").empty();
       $(".thumbStyle").empty();
-    });
+    // }
+  });
 
   // Mark turned "lexical" into a function so we can easily call it and pass in different classes of adLib-input
   function lexical(adlibArg){
@@ -144,71 +143,139 @@
         });
       });
     });
-}
+  }
 
 
 // New Story On Click Function
-    $(".newStoryButt").click(function(event){
-    lexical(".adLib-input2");
+$(".newStoryButt").click(function(event){
+  lexical(".adLib-input2");
 // Variables
-      var storiesRef = dataRef.ref().child("storyCounter");
-
-      var title = $(".titleUserInput");
-      var author = $(".authorUserInput");
-      var age = $(".ageUserInput");
-      adLib = $(".adLib-input2").val().trim();
-      console.log($(".adLib-input2").val().trim())
+var storiesRef = dataRef.ref().child("storyCounter");
+var title = $(".titleUserInput");
+var author = $(".authorUserInput");
+var age = $(".ageUserInput");
+adLib = $(".adLib-input2").val().trim();
+      // console.log($(".adLib-input2").val().trim())
       adLibArray.push(adLib);
 
 // Creates New Story Object in Firebase with a story ID.
-      storiesRef.once("value", function(snapshot) {
-        var storyCounter = snapshot.val();
-        storyCounter++;
-        var newStoryID = dataRef.ref().child('stories').orderByChild('id').equalTo(storyCounter);
+storiesRef.once("value", function(snapshot) {
+  var storyCounter = snapshot.val();
+  storyCounter++;
+        // var newStoryID = dataRef.ref().child('stories').orderByChild('id').equalTo(storyCounter);
         console.log(storyCounter);
-        console.log("newStoryID: "+newStoryID);
+        // console.log("newStoryID: "+newStoryID);
         storiesRef.set(storyCounter);
-        console.log(dataRef.ref().child("stories"));
+        // console.log(dataRef.ref().child("stories"));
 
         dataRef.ref().child("stories").push({
           id: storyCounter,
           title: "",
           author: "",
           storyPrompt: "",
-          adLibArray: [],
+          adLibArray: [""],
           dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
         
         // Click Event for Submit Story
         $(".commitNewStory").click(function(event){
-          newStoryID.on("value", function(snapshot) {
-            var storiesRef = dataRef.ref().child("stories");
-            storiesRef.once("value", function(snapshot) {
-              var stories = snapshot.val();
-              var currenStory = stories[storyIndex];
-        // if(storyIndex = "newStory"){
-        //   storyIndex = Object.keys(storiesRef).length+1
-        //   console.log(storyIndex)
-          // }
 
-        // else if(currenStory){
-          var storySentences = currenStory.adLibArray;
-          storySentences.push(adLib);
+          var userTitle = $(".userTitle").val();
+          console.log(userTitle);
+          var userAuthor = $(".userAuthor").val();
+          var adLib2 = $(".adLib-input2").val();
+          var newStoryID = dataRef.ref().child("stories").orderByChild('id').equalTo(storyCounter);
+          if(userTitle === ""){alert("Please Enter a Title")}
+            else if(userAuthor === ""){alert("Please Enter an Author")}
+              else if(adLib2 === ""){alert("Please Enter a Story Prompt")}
+                else{
+
 //This is where JOrdan Needs to find out how to look up object by Child(ID) in Firebase
 
-// Maybe limit queries like this will work?
-// curl 'https://console.firebase.google.com/project/friendscripts/database/data/stories.json?orderBy="id"&equalTo=NewStoryID&print=pretty'
-          storiesRef.child(storyCounter).set({
-            title: title,
-            author: author,
-            storyPrompt: "",
-            adLibArray: [],
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
+newStoryID.on("value", function(snapshot) {
+            // console.log(snapshot.val());
+            // console.log(Object.keys(snapshot.val())[0]);
+            var firebaseID = Object.keys(snapshot.val())[0];
+            firebaseID = firebaseID.toString();
+            // console.log(firebaseID);
+
+            
+
+            dataRef.ref().child("stories").child(firebaseID).update({
+              title: userTitle,
+              author: userAuthor,
+              storyPrompt: adLib2,
+              adLibArray: [""],
+            });
           });
-        });
-          });
-        });
+};
+});
+
+      });
 
 
 });
+
+$(".joinStoryButt").click(function(){
+  $(".editStoryDiv").empty();
+  var storyCounterRef = dataRef.ref().child("storyCounter");
+  storyCounterRef.once("value", function(counterSnapshot) {
+    var storyCounter = counterSnapshot.val();
+    console.log(storyCounter);
+    var numberOfStories = storyCounter - 10;
+    console.log(numberOfStories)
+    var storiesRef = dataRef.ref().child("stories");
+    storiesRef.once("value", function(storiesSnapshot) {
+      var storiesObj = storiesSnapshot.val();
+      for (var i = numberOfStories; i <= storyCounter; i += 1) {
+        var loopStoryID = dataRef.ref().child("stories").orderByChild('id').equalTo(i);
+        loopStoryID.once("value", function(storyWithId) {
+          var storiesWithLoopStoryId = storyWithId.val();
+          var storyKey = Object.keys(storiesWithLoopStoryId)[0];
+          console.log(storyKey)
+          var story = storiesWithLoopStoryId[storyKey];
+          console.log(story.title);
+          var newButt = $("<button>", {
+                text: story.title +": "+story.storyPrompt,
+                click: function() {
+                  var storyId = $(this).attr("data-story-id");
+                  storyIndex = storyId;
+                  console.log(storyId);
+                  console.log(story)
+                  storyPath = story;
+                  $(".prompt-Lib").empty();
+                  $(".mainContain").show();
+                  $(".landingPage").hide();
+                  $(".joinStory").hide();
+                  lexical(".adLib-input");
+                  // Show main container -done-
+                  // Populate it with the last ad lib -done-
+                  console.log(story.adLibArray[1])
+                  if(story.adLibArray[1]){
+                    var lastSentence = story.adLibArray.length - 1;
+                    $(".prompt-Lib").append(story.adLibArray[lastSentence]);
+                  }
+                  else{$(".prompt-Lib").append(story.storyPrompt)};
+                  // Set global story key to story Id -Done-
+
+                }
+              });
+          newButt.attr("data-story-id", storyKey);
+          $(".editStoryDiv").append(newButt)
+        });
+      }
+    });
+  });
 });
+
+
+
+
+
+$(".readStoryButt").click(function(){
+  
+  $(".prompt-Lib").empty();
+  $(".prompt-Lib").append(storyPath.storyPrompt+" "+storyPath.adLibArray.join(''))
+})
+
+
